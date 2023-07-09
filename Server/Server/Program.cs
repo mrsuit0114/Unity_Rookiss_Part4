6caf4cbccc65742fd1c798e3135723c69c67ac0b
HanDongHun <95519378+mrsuit0114@users.noreply.github.com>
2023-07-09 13:19:18 +0900

    Initial Commit
    환경설정 완료

---
+++ Server/Server/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}*/
+// 이전 버전에선 위와 같은 코드를 생성하며 의미는 아래와 같음
+// Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
