7d937fdd53ece11816b6ace74d31e9bdd7c8dadb
HanDongHun <95519378+mrsuit0114@users.noreply.github.com>
2023-07-23 15:31:11 +0900

    Unity Join #3

--- Server/Server/Packet/PacketHandler.cs
+++ Server/Server/Packet/PacketHandler.cs
@@ -11,16 +11,29 @@ using System.Threading.Tasks;
 class PacketHandler
 {
     // 어떤 세션에서 불렸고 어떤 패킷인지를 인자로 받는다
-    public static void C_ChatHandler(PacketSession session, IPacket packet)
+    public static void C_LeaveGameHandler(PacketSession session, IPacket packet)
     {
-        C_Chat chatPacket = packet as C_Chat;
         ClientSession clientSession = session as ClientSession;
 
         if (clientSession.Room == null)
             return;
 
         GameRoom room = clientSession.Room;
-        room.Push(() => room.Broadcast(clientSession, chatPacket.chat));
+        room.Push(() => room.Leave(clientSession));
     }
+    public static void C_MoveHandler(PacketSession session, IPacket packet)
+    {
+        C_Move movePacket = packet as C_Move;
+        ClientSession clientSession = session as ClientSession;
+
+        if (clientSession.Room == null)
+            return;
+
+        Console.WriteLine($"{movePacket.posX},{movePacket.posY},{movePacket.posZ}");
+
+        GameRoom room = clientSession.Room;
+        room.Push(() => room.Move(clientSession, movePacket));
+    }
+
 }
 
